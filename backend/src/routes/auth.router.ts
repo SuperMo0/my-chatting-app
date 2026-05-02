dc5123cf76c2920881231206b614b533eb1a2746
super <97206982+SuperMo0@users.noreply.github.com>
2026-05-02 17:17:18 +0300

    refactor: move validation in auth to middle wares and move prisma queris to the service layer

--- backend/src/routes/auth.router.ts
+++ backend/src/routes/auth.router.ts
@@ -1,17 +1,15 @@
 import { Router } from "express";
-import * as controller from '../controllers/auth.controller.js'
-import protect from '../middlewares/protect.js'
-
-
+import * as controller from '@/controllers/auth.controller.ts'
+import protect from '@/middlewares/protect.ts'
+import { validateBody } from "@/middlewares/validateBody.ts";
+import { loginBodySchema, signupBodySchema } from "super-chat-shared/auth";
 
 
 const router = Router();
 
+router.post('/login', validateBody(loginBodySchema), controller.login);
 
-router.post('/login', controller.login);
-
-
-router.post('/signup', controller.signup);
+router.post('/signup', validateBody(signupBodySchema), controller.signup);
 
 router.post('/logout', controller.logout);
 
---
+++ backend/src/services/auth.service.ts
@@ -0,0 +1,52 @@
+import prisma from '../lib/prisma.js';
+import type { SignupBody } from 'super-chat-shared/auth';
+import { Prisma } from '../../generated/prisma/index.js';
+
+export const safeUserSelection = {
+    id: true,
+    name: true,
+    email: true,
+    avatar: true,
+} satisfies Prisma.userSelect;
+
+export async function insertUser(user: SignupBody) {
+    const result = await prisma.user.create({
+        data: {
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            chats: {
+                connectOrCreate: {
+                    create: {
+                        id: "1",
+                        name: 'global'
+                    },
+                    where: {
+                        id: "1"
+                    }
+                }
+            }
+        },
+        select: safeUserSelection
+    });
+    return result;
+}
+
+export async function getUserByEmail(email: string) {
+    const result = await prisma.user.findFirst({
+        where: {
+            email: email
+        }
+    });
+    return result;
+}
+
+export async function getUserById(id: string) {
+    const result = await prisma.user.findUnique({
+        where: {
+            id: id
+        },
+        select: safeUserSelection
+    });
+    return result;
+}
